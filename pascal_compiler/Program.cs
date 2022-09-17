7730c5368c680a291c126e9ec215544b93367b1e
margo <margo.kalinina2002@mail.ru>
2022-09-17 21:47:38 +0500

    удалила комменты

--- pascal_compiler/Program.cs
+++ pascal_compiler/Program.cs
@@ -6,40 +6,10 @@ using System.Threading.Tasks;
 namespace pascal_compiler {
 	class Program
 	{
-		//struct Textposition
-		//{
-		//	uint linenumber; // номер строки
-		//	uint charnumber; // номер позиции в строке
-		//};
-
-		//struct Error { //таблица ошибок текущей строки
-		//	Textposition errorposition;
-		//	uint errorcode;
-
-		//};
-
-		////const int ERRMAX = 1e5; // наибольшее количество ошибок в строке
-		//static char ch; // текущая литера
-		//static short ErrInx; //количество обнаруженных ошибок в текущей строке
-		//const int MAXLINE = 100; // максимальная длина строки
-		//static Textposition positionNow;
-		//char line[MAXLINE]; //буфер ввода-вывода
-		//short errorOverFlow; // true, если превышает ERRMAX
-		//short lastInLine; // количество литер в текущей строке
-		////void nextch()
-		////{
-		////	if (positionNow.charnumber == lastInLine)
-		////	{
-
-		////	}
-		////}
-		////void error(unsigned errorcode, Textposition position)
-		////{
-
-		////}
+		
 		static void Main(string[] args)
 		{
-			//List<Error> errors;
+			
 			string filename;
 			
 			filename = Console.ReadLine();
