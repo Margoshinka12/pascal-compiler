90e3d53ae7e470b13a5129c0d99d13f687916cc7
margo <margo.kalinina2002@mail.ru>
2022-09-17 17:23:47 +0500

    Добавьте файлы проекта.

---
+++ pascal_compiler/IO module.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pascal_compiler
+{
+    public class IO_module
+    {
+        List<string> list;
+        int position;
+        string filename;
+        public IO_module(string filename)
+        {
+            position = 0;
+            list = new List<string>();
+            this.filename = filename;
+            ReadText();
+        }
+        public string GetNext()
+        {
+            if (position == list.Count) return "";
+            else
+            {
+                string s = list[position];
+                ++position;
+                return s;
+            }
+        }
+
+        private void WriteInList(string s)
+        {
+            string str = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                switch (s[i])
+                {
+                    case ';':
+                    case '(': case ')':
+                    case '{':
+                    case '}':
+                    case '[': case ']':
+                    case '\'':
+                    case '\"':
+                    case '.':
+                    case ',':
+                    case '/':
+                    case '*':
+                    case '+':
+                    case '-':
+                    case '=':
+                        {
+                            if (str != "") list.Add(str);
+                            str = "" + s[i];
+                            list.Add(str);
+                            str = "";
+                        }
+                        break;
+                    case ':':
+                        {
+
+                            if (str != "") list.Add(str);
+                            if (i < s.Length - 1 && s[i + 1] == '=')
+                            {
+                                list.Add(":="); i++;
+                            }
+                            else {
+                                list.Add(":"); 
+                            }
+                            str = "";
+                        }
+                        break;
+                    case '<':
+                        {
+                            if (str != "") list.Add(str);
+                            if (i < s.Length - 1 && s[i + 1] == '=')
+                            {
+                                list.Add("<="); i++;
+                            }
+                            else if (i < s.Length - 1 && s[i + 1] == '>')
+                            {
+                                list.Add("<>"); i++;
+                            }
+                            else
+                            {
+                                list.Add("<");
+                            }
+                            str = "";
+                        }
+                        break;
+                    case '>':
+                        {
+                            if (str != "") list.Add(str);
+                            if (i < s.Length - 1 && s[i + 1] == '=')
+                            {
+                                list.Add(">="); i++;
+                            }
+                            
+                            else
+                            {
+                                list.Add(">");
+                            }
+                            str = "";
+                        }
+                        break;
+                    case ' ':
+                        {
+                            if (str != "") list.Add(str); str = "";
+                        }
+                        break;
+                    default:
+                           str += s[i];
+                        if (i == s.Length - 1 && str != "") list.Add(str);
+                        break;
+
+                        
+                
+                }
+
+
+
+            }
+        }
+        private void ReadText()
+        {
+            try
+            {
+                using (StreamReader f = new StreamReader(filename))
+                {
+                    string s;
+                    while ((s = f.ReadLine()) != null)  WriteInList(s);
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error:" + e.Message);
+                return;
+            }
+
+        }
+    }
+}
---
+++ pascal_compiler/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace pascal_compiler {
+	class Program
+	{
+		//struct Textposition
+		//{
+		//	uint linenumber; // номер строки
+		//	uint charnumber; // номер позиции в строке
+		//};
+
+		//struct Error { //таблица ошибок текущей строки
+		//	Textposition errorposition;
+		//	uint errorcode;
+
+		//};
+
+		////const int ERRMAX = 1e5; // наибольшее количество ошибок в строке
+		//static char ch; // текущая литера
+		//static short ErrInx; //количество обнаруженных ошибок в текущей строке
+		//const int MAXLINE = 100; // максимальная длина строки
+		//static Textposition positionNow;
+		//char line[MAXLINE]; //буфер ввода-вывода
+		//short errorOverFlow; // true, если превышает ERRMAX
+		//short lastInLine; // количество литер в текущей строке
+		////void nextch()
+		////{
+		////	if (positionNow.charnumber == lastInLine)
+		////	{
+
+		////	}
+		////}
+		////void error(unsigned errorcode, Textposition position)
+		////{
+
+		////}
+		static void Main(string[] args)
+		{
+			//List<Error> errors;
+			string filename;
+			
+			filename = Console.ReadLine();
+			IO_module module = new IO_module(filename);
+			string s = "";
+            while (true)
+            {
+				s = module.GetNext();
+				if (s != "") Console.WriteLine(s);
+				else break;
+            }
+			
+
+		}
+	}
+}
