48f6cec0bf8923d4e0b017111d90f12ad6370db1
margo <margo.kalinina2002@mail.ru>
2022-10-19 18:21:12 +0500

    Строковый тип

---
+++ pascal_compiler/CStringVariant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pascal_compiler
+{
+    class CStringVariant : CVariant
+    {
+        public string value;
+        public CStringVariant(string value, IO_module IO)
+        {
+            if (value.Length > 255)
+               IO.errorsInLine.Add(new Error(IO.curPosition + 1 - value.Length, 11));
+            else
+                this.value = value;
+        }
+        public override void Show()
+        {
+            Console.Write(value + "{" + "String.");
+        }
+    }
+}
