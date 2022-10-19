def31ba72cc45b576b9f8efc4be1b00002af49d4
margo <margo.kalinina2002@mail.ru>
2022-10-19 18:21:06 +0500

    Целый тип

---
+++ pascal_compiler/CIntVariant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pascal_compiler
+{
+    class CIntVariant : CVariant
+    {
+        public int? value;
+        public CIntVariant(string value, IO_module IO)
+        {
+            try
+            {
+                this.value = Convert.ToInt32(value);
+            }
+            catch
+            {
+                IO.errorsInLine.Add(new Error(IO.curPosition + 1 - value.Length, 7));
+            }
+        }
+        public override void Show()
+        {
+            Console.Write(value + "{" + "Integer.");
+        }
+    }
+}
