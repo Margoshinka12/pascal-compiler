1bf261c6d6f96270a36578b2efc363a9f6a027aa
margo <margo.kalinina2002@mail.ru>
2022-10-19 17:55:42 +0500

    Константы

---
+++ pascal_compiler/CTokenValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pascal_compiler
+{
+    class CTokenValue : CToken
+    {
+        public CVariant value;
+        public CType type;
+        public CTokenValue(TokenType tt, CVariant value, CType type) : base(tt)
+        {
+            this.value = value;
+            this.type = type;
+        }
+        public override void Show()
+        {
+            value.Show();
+            Console.WriteLine(tt + "} ");
+        }
+    }
+}
