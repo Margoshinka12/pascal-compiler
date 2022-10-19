28e3e7daf68df793b69c50011c8260d9d74b164d
margo <margo.kalinina2002@mail.ru>
2022-10-19 18:21:21 +0500

    Идентификаторы

--- pascal_compiler/CTokenIdentifier.cs
+++ pascal_compiler/CTokenIdentifier.cs
@@ -9,18 +9,18 @@ namespace pascal_compiler
     class CTokenIdentifier : CToken
     {
         public string name;
-        public CType type;
+        
         public int offset;
         public CTokenIdentifier(TokenType tt, string name) : base(tt)
         {
             this.name = name;
         }
-        public CTokenIdentifier(TokenType tt, string name, CType type, int offset) : base(tt)
-        {
-            this.name = name;
-            this.type = type;
-            this.offset = offset;
-        }
+        //public CTokenIdentifier(TokenType tt, string name, int offset) : base(tt)
+        //{
+        //    this.name = name;
+           
+        //    this.offset = offset;
+        //}
         public override void Show()
         {
             Console.WriteLine(name + "{" + tt + "} ");
