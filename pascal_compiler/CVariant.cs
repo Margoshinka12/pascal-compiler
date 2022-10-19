bf65069cbe32a4e8bd95de92f65b93add3ade3fd
margo <margo.kalinina2002@mail.ru>
2022-10-19 18:22:20 +0500

    базовый класс типов

---
+++ pascal_compiler/CVariant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pascal_compiler
+{
+    class CVariant
+    {
+        public CVariant()
+        {
+        }
+        public virtual void Show()
+        {
+            Console.Write("");
+        }
+    }
+}
