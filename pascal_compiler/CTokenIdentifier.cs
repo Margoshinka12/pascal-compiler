388b1ccd9413f65c354829a88022c1c65e2182fc
margo <margo.kalinina2002@mail.ru>
2022-10-19 17:55:16 +0500

    Идентификаторы

---
+++ pascal_compiler/CTokenIdentifier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pascal_compiler
+{
+    class CTokenIdentifier : CToken
+    {
+        public string name;
+        public CType type;
+        public int offset;
+        public CTokenIdentifier(TokenType tt, string name) : base(tt)
+        {
+            this.name = name;
+        }
+        public CTokenIdentifier(TokenType tt, string name, CType type, int offset) : base(tt)
+        {
+            this.name = name;
+            this.type = type;
+            this.offset = offset;
+        }
+        public override void Show()
+        {
+            Console.WriteLine(name + "{" + tt + "} ");
+        }
+    }
+}
