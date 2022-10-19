3d927b8e56bdd8a2f01087d7b3ea1bcae69b3976
margo <margo.kalinina2002@mail.ru>
2022-10-19 18:20:21 +0500

    Булевый тип

---
+++ pascal_compiler/CBooleanVariant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pascal_compiler { 
+    class CBooleanVariant : CVariant
+    {
+        public bool value;
+        public CBooleanVariant(string value)
+        {
+            this.value = Convert.ToBoolean(value);
+        }
+        public override void Show()
+        {
+            Console.Write(value + "{" + "Boolean.");
+        }
+    }
+}
+
