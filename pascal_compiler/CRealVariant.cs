cde076aad211d556686790416aa0f1a39a3007dd
margo <margo.kalinina2002@mail.ru>
2022-10-19 18:20:51 +0500

    Вещественный тип

---
+++ pascal_compiler/CRealVariant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pascal_compiler
+{
+    class CRealVariant : CVariant
+    {
+        public double? value;
+        public CRealVariant(string value, IO_module IO)
+        {
+            try
+            {
+                this.value = Convert.ToDouble(value);
+            }
+            catch
+            {
+                IO.errorsInLine.Add(new Error(IO.curPosition + 1 - value.Length, 6));
+            }
+        }
+        public override void Show()
+        {
+            Console.Write(value + "{" + "Real.");
+        }
+    }
+}
+
