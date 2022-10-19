0c4fdcc108048af7cc17d1132ac98179d5edf1c9
margo <margo.kalinina2002@mail.ru>
2022-10-19 18:20:59 +0500

    Символьный тип

---
+++ pascal_compiler/CCharVariant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pascal_compiler
+{
+    class CCharVariant : CVariant
+    {
+        public char value;
+        public CCharVariant(string value)
+        {
+            this.value = value[0];
+        }
+        public override void Show()
+        {
+            Console.Write(value + "{" + "Char.");
+        }
+    }
+}
