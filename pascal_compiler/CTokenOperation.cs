293d85309ef0c2b6186c2b023072e7b6f82ffe8e
margo <margo.kalinina2002@mail.ru>
2022-10-19 17:55:29 +0500

    Операторы

---
+++ pascal_compiler/CTokenOperation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pascal_compiler
+{
+    class CTokenOperation : CToken
+    {
+        public int code;
+        public CTokenOperation(TokenType tt, int code) : base(tt)
+        {
+            this.code = code;
+        }
+        public override void Show()
+        {
+            string name = "";
+            foreach (KeyValuePair<string, int> pair in LexicalAnalyzer.keyWords)
+                if (code == pair.Value)
+                {
+                    name = pair.Key;
+                    break;
+                }
+            Console.WriteLine(name + "{" + tt + "} ");
+        }
+    }
+}
